45efaedeac04009f933bd35585170fba01a74a37
jskriptus <skriptus@bk.ru>
2020-04-02 01:54:02 +0500

    Выполнил обязательное домашнее задание пятого урока

--- script.js
+++ script.js
@@ -1,36 +1,62 @@
 'use strict';
 
-let money = +prompt('Ваш месячный доход?', '10000'), // Спрашиваем у пользователя месячный доход
+let money, // Спрашиваем у пользователя месячный доход
     income = 'фриланс', // Дополнительный доход
     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', ''), // Спрашиваем у пользователя возможные дополнительные рассходы
     deposit = confirm('Есть ли у вас депозит в банке?'), // Спрашиваем пользователя есть ли у него депозит в банке
     mission = 100500, // Какую сумму необходимо накопить (цель)
     period = 6, // За какой срок
-    budgetDay; // Бюджет на день
+    budgetDay, // Бюджет на день
+    expenses = []; // Записываем в массив обязательные статьи рассходов
 
-let expensesOne = prompt('Введите обязательную статью расходов'), // Спрашиваем пользователя название первой обязательной статьи рассходов
-    amountOne = +prompt('Во сколько это обходится?', '1000'), // Спрашиваем пользователя сумму во сколько обходится первая обязательная статья рассходов
-    expensesTwo = prompt('Введите обязательную статью расходов'), // Спрашиваем пользователя название второй обязательной статьи рассходов
-    amountTwo = +prompt('Во сколько это обходится?', '1500'); // Спрашиваем пользователя сумму во сколько обходится вторая обязательная статья рассходов
+
+const isNumber = (n) => {
+    return !isNaN(parseInt(n)) && isFinite(n);
+};
+
+const start = () => {
+    do {
+        money = +prompt('Ваш месячный доход?', '10000');
+    }
+    while (!isNumber(money));
+};
+
+start();
 
 
 const showTypeOf = (item) => {
     console.log(typeof (item));
 };
 
-const getExpensesMonth = (mandatoryOne, mandatoryTwo) => { // Функция возвращает сумму всех обязательных расходов за месяц
-    return mandatoryOne + mandatoryTwo;
-}
+const getExpensesMonth = () => { // Функция возвращает сумму всех обязательных расходов за месяц
+    let result = 0;
+
+    for (let i = 0; i < 2; i++) {
+
+        expenses[i] = prompt('Введите обязательную статью расходов');
+
+        do {
+            result += +prompt('Во сколько это обходится?', '1000');
+        }
+        while (!isNumber(result));
+    }
+
+    return result;
+};
+
+let expensesAmount = getExpensesMonth();
 
 const getAccumulatedMonth = (income, expenses) => { // Функция возвращает Накопления за месяц (Доходы минус расходы)
     return income - expenses;
-}
+};
 
-const accumulatedMonth = getAccumulatedMonth(money, getExpensesMonth(amountOne, amountTwo)); // присваиваем переменной результат исполнения функции которая считает накопления за месяц 
+const accumulatedMonth = getAccumulatedMonth(money, expensesAmount); // присваиваем переменной результат исполнения функции которая считает накопления за месяц 
 
 const getTargetMonth = (goal, accumulated) => { // Подсчитывает за какой период будет достигнута цель, зная результат месячного накопления accumulatedMonth
     return Math.ceil(goal / accumulated);
-}
+};
+
+const targetMonth = getTargetMonth(mission, accumulatedMonth);
 
 const getStatusIncome = () => {
     if (budgetDay >= 1200) { // Определяем уровень дохода пользователя 
@@ -50,11 +76,16 @@ showTypeOf(money);
 showTypeOf(income);
 showTypeOf(deposit);
 
-console.log('Сумма обязательных рассходов за месяц: ' + getExpensesMonth(amountOne, amountTwo));
+console.log('Сумма обязательных рассходов за месяц: ' + expensesAmount);
 
 console.log(addExpenses.toLowerCase().split(","));
 
-console.log('Цель будет достигнута за: ' + getTargetMonth(mission, accumulatedMonth) + ' месяца(ев)');
+if (targetMonth > 0) {
+    console.log('Цель будет достигнута за: ' + targetMonth + ' месяца(ев)');
+
+} else {
+    console.log('Цель не будет достигнута');
+}
 
 console.log('Бюджет на день: ' + budgetDay);
 
