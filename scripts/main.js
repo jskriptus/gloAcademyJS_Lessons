53294212f0e863df102ae203cb8fa36b9c2c8b80
jskriptus <skriptus@bk.ru>
2020-04-28 23:57:38 +0100

    Show menu and popup + animation popup

--- scripts/main.js
+++ scripts/main.js
@@ -48,4 +48,63 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     countTimer('02 july 2020');
+
+    // Меню
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItem = menu.querySelectorAll('ul > li');
+
+        const showOrClosedMenu = () => {
+            menu.classList.toggle('active-menu');
+            // if (!menu.style.transform || menu.style.transform === 'translate(-100%)') {
+            //     menu.style.transform = 'translate(0)';
+            // } else {
+            //     menu.style.transform = 'translate(-100%)';
+            // }
+        };
+
+        btnMenu.addEventListener('click', showOrClosedMenu);
+        closeBtn.addEventListener('click', showOrClosedMenu);
+
+        menuItem.forEach(item => item.addEventListener('click', showOrClosedMenu));
+    };
+
+    toggleMenu();
+
+    // popup
+    const togglePopup = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupContent = popup.querySelector('.popup-content'),
+            // formBtn = popup.querySelector('.form-btn'),
+            popupClose = popup.querySelector('.popup-close');
+
+        const showOrHiddenPopup = () => {
+
+            if (!popup.style.display || popup.style.display === 'none') {
+                let translate = -150;
+
+                const animate = setInterval(() => {
+                    popup.style.display = 'block';
+                    popupContent.style.transform = `translate(${translate += 2}%)`;
+                    console.log(screen.width);
+                    if (translate === 0 || screen.width === 768) {
+                        clearInterval(animate);
+                    }
+                }, 2);
+            } else {
+                popup.style.display = 'none';
+            }
+        };
+
+        popupBtn.forEach(item => {
+            item.addEventListener('click', showOrHiddenPopup);
+        });
+
+        popupClose.addEventListener('click', showOrHiddenPopup);
+    };
+
+    togglePopup();
 });
